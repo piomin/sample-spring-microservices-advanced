516583a9dc2ca1401fe710e78db7c12abf686683
piotr.minkowski <piotr.minkowski@gmail.com>
2017-04-11 17:31:13 +0200

    swagger ui

--- gateway-service/src/main/java/pl/piomin/microservices/advanced/gateway/Application.java
+++ gateway-service/src/main/java/pl/piomin/microservices/advanced/gateway/Application.java
@@ -3,7 +3,9 @@ package pl.piomin.microservices.advanced.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -15,4 +17,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
+	  @Bean
+	  UiConfiguration uiConfig() {
+	    return new UiConfiguration(
+	        "validatorUrl",
+	        "list",       // docExpansion          => none | list
+	        "alpha",      // apiSorter             => alpha
+	        "schema",     // defaultModelRendering => schema
+	        UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
+	        false,        // enableJsonEditor      => true | false
+	        true,         // showRequestHeaders    => true | false
+	        60000L);      // requestTimeout => in milliseconds, defaults to null (uses jquery xh timeout)
+	  }
 }
