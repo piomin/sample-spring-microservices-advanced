e1766a8a9754fc5a7401e1e3d1e40bc0b07fae0f
piotr.minkowski <piotr.minkowski@gmail.com>
2017-07-11 15:30:23 +0200

    add config server

---
+++ config-server/src/main/java/pl/piomin/microservices/advanced/config/ConfigServer.java
@@ -0,0 +1,15 @@
+package pl.piomin.microservices.advanced.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer.class, args);
+	}
+
+}
