fd801c55869c79fff8ddeaad32766ee99cf6b8d8
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-07-13 23:29:41 +0200

    fixes

--- config-server/src/main/java/pl/piomin/microservices/advanced/config/ConfigServer.java
+++ config-server/src/main/java/pl/piomin/microservices/advanced/config/ConfigServer.java
@@ -2,10 +2,12 @@ package pl.piomin.microservices.advanced.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServer {
 
 	public static void main(String[] args) {
