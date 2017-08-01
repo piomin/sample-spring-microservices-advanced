7abb8b8c4f63a842008a20bc74406537c183c8c1
piotr.minkowski <piotr.minkowski@gmail.com>
2017-08-01 15:05:52 +0200

    changes

--- account-service/src/test/java/pl/piomin/microservices/advanced/account/api/AccountApiFullTest.java
+++ account-service/src/test/java/pl/piomin/microservices/advanced/account/api/AccountApiFullTest.java
@@ -1,39 +1,58 @@
 package pl.piomin.microservices.advanced.account.api;
 
-import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
-import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
-import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+import java.util.logging.Logger;
 
+import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
+import io.specto.hoverfly.junit.core.HoverflyConfig;
 import io.specto.hoverfly.junit.rule.HoverflyRule;
 import pl.piomin.microservices.advanced.account.Application;
 import pl.piomin.microservices.advanced.account.model.Account;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class}, webEnvironment = WebEnvironment.DEFINED_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AccountApiFullTest {
 
-//	TestRestTemplate template = new TestRestTemplate();
+	protected Logger logger = Logger.getLogger(AccountApiFullTest.class.getName());
 	
-	RestTemplate template = new RestTemplate();
+	@Autowired
+	TestRestTemplate template;
 	
     @ClassRule
-    public static HoverflyRule hoverflyRule = HoverflyRule.inCaptureOrSimulationMode("account.json").printSimulationData();
+    public static HoverflyRule hoverflyRule = HoverflyRule.inCaptureOrSimulationMode("account.json", HoverflyConfig.configs().proxyLocalHost()).printSimulationData();
     
     @Test
     public void addAccountTest() {    	
     	Account a = new Account("1234567890", 1000, "1");
-    	ResponseEntity<Account> r = template.postForEntity("http://localhost:2222/accounts", a, Account.class);
-    	System.out.println(r.getBody().getId());
+    	ResponseEntity<Account> r = template.postForEntity("/accounts", a, Account.class);
+    	Assert.assertNotNull(r.getBody().getId());
+    	logger.info("New account: " + r.getBody().getId());
+    }
+    
+    @Test
+    public void findAccountByNumberTest() {
+    	Account a = template.getForObject("/accounts/number/{number}", Account.class, "1234567890");
+    	Assert.assertNotNull(a);
+    	logger.info("Found account: " + a.getId());
+    }
+    
+    @Test
+    public void findAccountByCustomerTest() {
+    	Account[] a = template.getForObject("/accounts/customer/{customer}", Account[].class, "1");
+    	Assert.assertTrue(a.length > 0);
+    	logger.info("Found accounts: " + a);
     }
     
 }
---
+++ customer-service/src/test/java/pl/piomin/microservices/advanced/customer/api/CustomerControllerTest2.java
@@ -0,0 +1,51 @@
+package pl.piomin.microservices.advanced.customer.api;
+
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.specto.hoverfly.junit.rule.HoverflyRule;
+import pl.piomin.microservices.advanced.customer.model.Customer;
+import pl.piomin.microservices.advanced.customer.model.CustomerType;
+
+import static io.specto.hoverfly.junit.core.SimulationSource.*;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.*;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.*;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CustomerControllerTest2 {
+
+	@Autowired
+	TestRestTemplate template;
+	
+    @ClassRule
+    public static HoverflyRule hoverflyRule = HoverflyRule.inCaptureOrSimulationMode("customer.json").printSimulationData();
+    
+	@Test
+	public void addCustomerTest() {
+		Customer c = new Customer("1234567890", "Jan Testowy", CustomerType.INDIVIDUAL);
+		c = template.postForObject("/customers", c, Customer.class);
+	}
+	
+	@Test
+	public void findCustomerWithAccounts() {
+		Customer c = template.getForObject("/customers/pesel/{pesel}", Customer.class, "1234567890");
+		Customer cc = template.getForObject("/customers/{id}", Customer.class, c.getId());
+		Assert.assertTrue(cc.getAccounts().size() > 0);
+	}
+}
