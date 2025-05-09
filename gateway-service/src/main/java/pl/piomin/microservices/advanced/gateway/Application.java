31d2a91f9bc61135da0d44e5fcfe0ecb90e1b66b
piomin <piotr.minkowski@gmail.com>
2025-05-09 14:15:18 +0200

    swagger docs fix

--- gateway-service/src/main/java/pl/piomin/microservices/advanced/gateway/Application.java
+++ gateway-service/src/main/java/pl/piomin/microservices/advanced/gateway/Application.java
@@ -1,15 +1,20 @@
 package pl.piomin.microservices.advanced.gateway;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 
 @SpringBootApplication
 public class Application {
@@ -22,15 +27,17 @@ public class Application {
 	RouteDefinitionLocator locator;
 
 	@Bean
-	public List<GroupedOpenApi> apis() {
-		List<GroupedOpenApi> groups = new ArrayList<>();
+	@Lazy(false)
+	public Set<SwaggerUrl> apis(RouteDefinitionLocator locator, SwaggerUiConfigProperties swaggerUiConfigProperties) {
+		Set<SwaggerUrl> urls = new HashSet<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-		assert definitions != null;
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
-			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
+			SwaggerUrl swaggerUrl = new SwaggerUrl(name, DEFAULT_API_DOCS_URL+"/" + name, null);
+			urls.add(swaggerUrl);
 		});
-		return groups;
+		swaggerUiConfigProperties.setUrls(urls);
+		return urls;
 	}
 
 }
